bdb535b4ce972b99cbd1c8fb2aa30c3bf795dacd
E - Coders (Aasheesh Agarwal) <83082760+e-coders@users.noreply.github.com>
2021-10-31 22:13:40 +0530

    Update DepInstaller.py

--- DepInstaller.py
+++ DepInstaller.py
@@ -6,14 +6,7 @@ print('###############################')
 
 print('\nInstalling dependencies...')
 
-os.system('python -m pip install playsound')
-os.system('python -m pip install pyautogui')
-os.system('python -m pip install gtts')
-os.system('python -m pip install pywhatkit')
-os.system('python -m pip install PyAudio')
-os.system('python -m pip install SpeechRecognition')
-os.system('python -m pip install chatterbot-corpus')
-os.system('python -m pip install nltk')
+os.system('python -m pip install playsound pyautogui gtts pywhatkit PyAudio SpeechRecognition chatterbot-corpus nltk')
 
 print('###############################')
 print('#JARVISInstallation successful#')
